5368990c1602f6903cf6f2d43769581106c1161f
saAhammad <saahammad94@gmail.com>
2025-08-22 12:01:46 +0600

    remove dotenv

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
 import express from "express";
-import nodemailer from "nodemailer";
 
 const app = express();
-dotenv.config();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
