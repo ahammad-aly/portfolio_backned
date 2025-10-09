ef1383acd1361053543c4149c56e1ae402758758
saAhammad <saahammad94@gmail.com>
2025-10-09 15:13:58 +0600

    add resend

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import serverless from "serverless-http";
 const app = express();
 
 dotenv.config();
+const port = 4000;
 
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
@@ -16,6 +17,10 @@ app.use("/api/email", emailRoute);
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there, this is from portfolio backend</h2>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is runing at port : http://localhost:${port}`);
 });
 export const handler = serverless(app);
 export default app;
--- src/controlers/email.controller.js
+++ src/controlers/email.controller.js
@@ -1,20 +1,29 @@
 import { Resend } from "resend";
 
 export async function sendEmail(req, res) {
-  const resend = new Resend(`${process.env.EMAIL_PASS}`);
-  const { name, email, message } = req.body;
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const { name, email, message } = req.body;
 
-  if ([name, email, message].some((fields) => fields?.trim() === "")) {
+    if ([name, email, message].some((fields) => fields?.trim() === "")) {
+      return res
+        .status(400)
+        .json({ stat: 400, msgerr: "All fields are required" });
+    }
+    await resend.emails.send({
+      from: `${name} <portfolio_email@resend.dev>`,
+      to: process.env.EMAIL_ADDRESS,
+      subject: "Hello Ahammad you have an email from your portfolio.",
+      html: `<p><strong>From:</strong> ${email}</p><p>${message}</p>`,
+    });
+
     return res
-      .status(400)
-      .json({ stat: 400, msgerr: "All fields are required" });
+      .status(200)
+      .json({ email: email, msg: "Message send successfully" });
+  } catch (error) {
+    console.error("Resend error:", error);
+    return res.status(500).json({ success: false, error });
   }
-  resend.emails.send({
-    from: email,
-    to: process.env.EMAIL_ADDRESS,
-    subject: "Hello Ahammad you have an email from your portfolio.",
-    html: message,
-  });
 }
 
 // import nodemailer from "nodemailer";
